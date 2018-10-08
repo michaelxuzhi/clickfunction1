cb94eee903d12b421219b1b0c5850bf70de706d6
Michaelxuzhi <michaelxuzhi@foxmail.com>
2018-10-08 17:16:32 +0800

    Add files via upload

---
+++ hello world触发事件/page/hello/hello.js
@@ -0,0 +1,82 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src:"https://apd-6729dcc2d7ac75b6b8f34a5b59f73655.v.smtcdns.com/sportsts.tc.qq.com/AFtSjIOdtGUTwJJ9NisOlW5tVBB-XwJ5_kTrAC-JtvOw/j9sp3v4voVlmoOLz9wvHR1SkfNaa11ikyFMo1ppFwdcHEOXrL8Q1_GW1pvjBc_fb0TPqrBjEXKgKObNOt-w4FQmkhJlrN1jE9pNf6XBs9iv68BBzh9DwiB9EB6RxA-hoDNvjxRHEsjeBBRmjNQLXnNvd84c1jiac/013_e00265zgpjq.321002.1.ts?index=13&start=127680&end=137440&brs=12953952&bre=13827963&ver=4",
+    icon:{
+      type:"success",
+      size:"30",
+      color:"red"
+    },
+    progress:{
+      percent:"40",
+      color:"pink",
+      active:true,
+      show_info:false,
+      stroke_width:"10"
+    }
+  },
+
+  click:function(){
+    this.setData({
+      hello:"棒棒"
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
